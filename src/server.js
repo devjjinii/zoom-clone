36a226769aa227e391d8d2d449081fd899cae752
devjjinii <devjjinii@naver.com>
2021-09-04 23:06:09 +0900

    install websocket module

--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import http from "http";
+import WebSocket from "ws";
 import express from "express";
 
 const app = express();
@@ -7,8 +9,16 @@ app.set("view engine","pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
 
-//router
+//router 설정
 app.get("/", (req,res) => res.render("home"));
+app.get("/*", (req,res) => res.redirect("/")); //home만 사용
 
 const handleListen = () => console.log(`http://localhost:3000`);
-app.listen(3000, handleListen);
+// app.listen(3000, handleListen);
+
+
+// 아래와 같이 하는 이유 : 같은 포트에 두개의 서버를 띄우기 위해
+const server = http.createServer(app); // http server 생성
+const wss = new WebSocket.Server({server}); // Ws server 생성 (http 서버 전달 )
+
+server.listen(3000, handleListen);
