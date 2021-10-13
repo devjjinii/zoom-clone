f920346dcf0992154e8da23ffd64c35f05cf8484
devjjinii <devjjinii@naver.com>
2021-10-13 13:56:43 +0900

    카메라 선택옵션 추가

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -3,27 +3,64 @@ const socket = io();
 const myFace = document.getElementById("myFace");
 const muteBtn = document.getElementById("mute");
 const cameraBtn= document.getElementById("camera");
+const camerasSelect = document.getElementById("cameras");
 
 let myStream;
 let muted = false;
 let cameraOff = false;
 
-async function getMedia() {
-    try {
-        myStream = await navigator.mediaDevices.getUserMedia({
-            audio: true,
-            video: true,
+async function getCameras() {
+    try{
+        const devices = await navigator.mediaDevices.enumerateDevices();
+        // console.log(devices);
+        const cameras = devices.filter(device => device.kind === "videoinput");
+        // console.log(cameras);
+        const currentCamera = myStream.getVideoTracks()[0];
+        cameras.forEach(camera => {
+            const option = document.createElement("option");
+            option.value = camera.deviceId;
+            option.innerText = camera.label;
+            if(currentCamera.label == camera.label) {
+                option.selected = true;
+            }
+            camerasSelect.appendChild(option);
         });
-        console.log(myStream);
-        myFace.srcObject = myStream;
     } catch(e) {
         console.log(e);
     }
 }
 
-getMedia();
+async function getMedia(deviceId) {
+    const initialConstrains = {
+        audio: true,
+        video: { facingMode : "user"},
+    };
+    const cameraConstraints = {
+        audio: true,
+        video: { deviceId: { esact: deviceId } },
+    };
+
+    try {
+        myStream = await navigator.mediaDevices.getUserMedia(
+            deviceId ? cameraConstraints : initialConstrains
+        );
+        console.log(myStream);
+        myFace.srcObject = myStream;
+        if (!deviceId) {
+            await getCameras();  // 
+        }
+    } catch(e) {
+        console.log(e);
+    }
+}
+
+getMedia(); // deviceId 없이 호출
 
 function handleMuteClick() {
+    // console.log(myStream.getAudioTracks());
+    myStream
+        .getAudioTracks()
+        .forEach((track) => (track.enabled = !track.enabled));
     if(!muted) {
         muteBtn.innerText = "Unmute";
         muted = true;
@@ -34,6 +71,9 @@ function handleMuteClick() {
 }
 
 function handleCameraClick() {
+    myStream
+    .getVideoTracks()
+    .forEach((track) => (track.enabled = !track.enabled));
     if(cameraOff) {
         cameraBtn.innerText = "Turn Camera Off";
         cameraOff = false;
@@ -43,5 +83,11 @@ function handleCameraClick() {
     }
 }
 
+async function handleCameraChange() {
+    // console.log(camerasSelect.value);
+    await getMedia(camerasSelect.value);
+}
+
 muteBtn.addEventListener("click", handleMuteClick);
 cameraBtn.addEventListener("click", handleCameraClick);
+camerasSelect.addEventListener("input", handleCameraChange);
